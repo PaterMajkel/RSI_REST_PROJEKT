5bff53bb785d802bee877168d7f9e24b1848ad5d
Michał Bołtowicz <micbo26@gmail.com>
2024-06-17 00:23:24 +0200

    openapi

--- RSI_REST_SERVER/Program.cs
+++ RSI_REST_SERVER/Program.cs
@@ -7,8 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Rsi Api", Version = "v1" });
+});
 var app = builder.Build();
 
 app.UseMiddleware<MyResponseMiddleware>();
@@ -18,8 +20,11 @@ app.UseMiddleware<BasicAuthMiddleware>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseOpenApi();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Rsi Api");
+    });
 }
 
 app.UseHttpsRedirection();
